481e13d7835ef1b4019726e9d3bca5d229eeeeaa
zhongleilz <zhongleilz@icloud.com>
2025-08-12 16:03:03 +0100

    Add some prepare .sh file

---
+++ prepare/prepare_clip.sh
@@ -0,0 +1,5 @@
+mkdir -p deps/
+cd deps/
+git lfs install
+git clone https://huggingface.co/openai/clip-vit-large-patch14
+cd ..
---
+++ prepare/prepare_t5.sh
@@ -0,0 +1,5 @@
+mkdir -p deps/
+cd deps/
+git lfs install
+git clone https://huggingface.co/sentence-transformers/sentence-t5-large
+cd ..
